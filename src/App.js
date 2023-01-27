0df0f456312d77a4fa3735f8bb98f060b4e0d142
dtromans <29309408+dtromans@users.noreply.github.com>
2023-01-26 21:45:09 -0500

    Implementing search filter (WIP)

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
 		this.state = {
 			monsters: [],
 		};
+		console.log('constructor');
 	}
 
 	componentDidMount() {
+		console.log('componentDidMount')
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
 			.then((users) => this.setState(() => {
@@ -24,8 +26,17 @@ class App extends Component {
 	}
 
 	render() {
+		console.log('render')
 		return (
 			<div className="App">
+				<input 
+					className='search-box' 
+					type='search' 
+					placeholder='search monsters' 
+					onChange={(event) => {
+						console.log(event.target.value);
+					}}
+				/>
 				{
 					this.state.monsters.map((monster) => {
 						return (
@@ -35,7 +46,12 @@ class App extends Component {
 						);
 					})
 				}
+
+				<button>
+					Text
+				</button>
 			 </div>
+
 		);
 	}
 
